e84faaf1978a57859b87ffbc1a1b24cf706ce6b5
Camelia Milut <milut.camelia@gmail.com>
2023-10-24 17:58:56 +0300

    Initial commit

---
+++ src/main/java/com/stones/capturingstones/CapturingStonesApplication.java
@@ -0,0 +1,13 @@
+package com.stones.capturingstones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapturingStonesApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(CapturingStonesApplication.class, args);
+    }
+
+}
