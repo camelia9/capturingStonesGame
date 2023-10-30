d8dfd6bf090ddefe8bed9a757f2db47f8c054081
Camelia Milut <milut.camelia@gmail.com>
2023-10-30 14:35:40 +0200

    Added player info and changed display

--- capturingStonesFront/captureStones-game/src/app/capture-stones-game/capture-stones-game.component.ts
+++ capturingStonesFront/captureStones-game/src/app/capture-stones-game/capture-stones-game.component.ts
@@ -8,7 +8,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CaptureStonesGameComponent implements OnInit {
 
-  board: number[] = [];
+  boardONE: number[] = [];
+  boardTWO: number[] = [];
+  status: string = ""
+  player: string = ""
+  isLast: number = 6 ;
 
 
   constructor(private http: HttpClient) {}
@@ -16,22 +20,34 @@ export class CaptureStonesGameComponent implements OnInit {
   ngOnInit(): void {
     this.fetchGameStatus();
     this.fetchBoardValues();
+    this.fetchActivePlayer();
   }
 
   fetchGameStatus() {
-    this.http.get<string>(' http://localhost:8080/captureStones/game-status')
+    this.http.get<any>(' http://localhost:8080/captureStones/game-status')
       .subscribe(status => {
         // Handle the game status, e.g., update your component state
+        this.status = status.status;
         console.log('Game Status:', status);
       });
   }
 
+  fetchActivePlayer() {
+    this.http.get<any>(' http://localhost:8080/captureStones/player-turn')
+      .subscribe(player => {
+        // Handle the game status, e.g., update your component state
+        this.player = player.player;
+        console.log('Active Player:', player);
+      });
+  }
+
     makeMove(pitIndex: number) {
-      this.http.post('http://localhost:8080/captureStones/make-move', { playerNumber: 'ONE', smallPit: pitIndex })
+      this.http.post('http://localhost:8080/captureStones/make-move', { playerNumber: this.player, smallPit: pitIndex })
         .subscribe(() => {
           // After making a move, you can fetch the updated game status
           this.fetchGameStatus();
         });
+      this.fetchBoardValues();
     }
 
   fetchBoardValues() {
@@ -39,8 +55,8 @@ export class CaptureStonesGameComponent implements OnInit {
       .subscribe(data => {
         console.log(data.ONE); // Access the values for Player "ONE"
         console.log(data.TWO); // Access the values for Player "TWO"
-        this.board = data.ONE.concat(data.TWO);
-
+        this.boardONE = data.ONE;
+        this.boardTWO = data.TWO;
       });
   }
 
